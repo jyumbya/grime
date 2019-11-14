2d0c6182333be5848414a8cedaa2f6ceabb4861d
John Mutua <jyumbya@yahoo.com>
2019-11-14 10:23:50 +0300

    added argument descriptions

--- R/soilExtractor.R
+++ R/soilExtractor.R
@@ -1,27 +1,31 @@
 require(raster)
 require(GSIF)
 
-#' @title Extract soil property data from SoilGrids.
+#' @title Create a spatial point dataframe
 #'
-#' @description Create a spatial point dataframe.
+#' @description Create a spatial point dataframe
 #'
 #' @param y Latitude in decimal degrees
 #'
 #' @param x Longitude in decimal degrees
 #'
-#' @param z
+#' @param z Character containing name of location, ID etc
 #'
-#' @return spatialpointsdataframe
+#' @importFrom sp SpatialPoints
+#'
+#' @importFrom sp CRS
 #'
 #' @examples
+#' library(grime)
 #' x <- 37.437971
 #' y <- -1.86698
 #' z <-"Matwiku"
-#' pnts <- def_loc(x,y, z)
+#' pnts <- defineLocation(x,y, z)
+#' pnts
 #'
 #' @export
 
-def_loc <- function(x, y, z) {
+defineLocation <- function(x, y, z) {
 
   # x=long, y=lat, z=id(string)
   x <- as.numeric(x)  # convert x from characters to numeric
@@ -30,45 +34,67 @@ def_loc <- function(x, y, z) {
 
   pts <- data.frame(lon = x, lat = y, id = z)  # dataframe
 
-  # create a spatial point dataframe
-  SpatialPointsDataFrame(pts[, c("lon", "lat")], data = pts, proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+  # create a spatialpoints
+  SpatialPoints(pts[, c("lon", "lat")], proj4string = CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
 
 }
 
-#' @description Create class for SoilGrids REST API.
+
+#' @title Create class for SoilGrids REST API
 #'
-#' @param a
+#' @description Create class for SoilGrids REST API
+#'
+#' @param a Vector containing list of soil properties to extract (ORCDRC, PHIHOX)
 #'
 #' @return list
 #'
+#' @importFrom GSIF REST.SoilGrids
+#'
 #' @examples
+#' library(grime)
 #' a <- c("ORCDRC", "PHIHOX")
-#' slist <- soil_list(a)
+#' slist <- defineSoilProperties(a)
+#' slist
 #'
 #' @export
 
-soil_list <- function(...) {
+defineSoilProperties <- function(...) {
 
   GSIF::REST.SoilGrids(c(...))
 
 }
 
-#' @description Extract soil properties from soil grids.
+#' @title Extract soil properties from soil grids
 #'
-#' @param slist
+#' @description Extract soil properties from soil grids
 #'
-#' @param pnts
+#' @param slist Object of class "list"; contains parameters or REST.SoilGrids query
+#'
+#' @param pnts  Geometry (locations) of the queries
 #'
 #' @return dataframe
 #'
-#' @examples
+#' @importFrom sp SpatialPoints
 #'
-#' soil_properties <- extract_soils(slist, pnts)
+#' @importFrom sp CRS
+#'
+#' @importFrom sp over
+#'
+#' @examples
+#' library(grime)
+#' x <- 37.437971
+#' y <- -1.86698
+#' z <-"Matwiku"
+#' pnts <- defineLocation(x,y,z)
+#' a <- c("ORCDRC", "PHIHOX")
+#' slist <- defineSoilProperties(a)
+#' soil_properties <- extractSoilsProperties(slist, pnts)
+#' soil_properties
 #'
 #' @export
 
-extract_soils <- function(x, y) {
+extractSoilsProperties <- function(x, y) {
 
-  over(x, y)
+  sp::over(x, y)
 
 }
